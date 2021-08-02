0a22110310189874d60503dfc413f8ace8df71a2
GRandAMi1 <Amitshendge1990@gmail.com>
2021-08-02 16:23:34 +0530

    Changes made to make it simple program

--- Assignment Day 5/src/com/blz/basiccoreprograming/10th_program_modular/TenthProgramModular.java
+++ Assignment Day 5/src/com/blz/basiccoreprograming/10th_program_modular/TenthProgramModular.java
@@ -5,24 +5,27 @@ import java.util.Scanner;
 public class TenthProgramModular {
     public static final Scanner scanner = new Scanner(System.in);
     public static int userInput;
-    public static int value = totalNumbers();
-    public static int[] dataArr = new int[userInput+1];
-    public static int totalNumbers(){
+//    public static int value = totalNumbers();
+//    public static int[] dataArr = new int[userInput+1];
+//    public static int totalNumbers(){
+//        System.out.print("How many numbers you want to Enter : ");
+//        userInput = scanner.nextInt();
+//        return 0;
+//    }
+    public static void main(String[] args) {
         System.out.print("How many numbers you want to Enter : ");
         userInput = scanner.nextInt();
-        return 0;
+        int[] dataArr = new int[userInput+1];
+        collectData(dataArr);
     }
-    public static void main(String[] args) {
-        collectData();
-    }
-    public static void collectData(){
+    public static void collectData(int[] dataArr){
         for (int i = 1; i <= userInput; i++) {
             System.out.print(i+". Enter number : ");
             dataArr[i]=scanner.nextInt();
         }
-        largestNumber();
+        largestNumber(dataArr);
     }
-    public static void largestNumber(){
+    public static void largestNumber(int[] dataArr){
         int temp = dataArr[1];
         for (int i = 2; i <= userInput; i++) {
             if (dataArr[i]>temp){
@@ -30,9 +33,9 @@ public class TenthProgramModular {
             }
         }
         System.out.println("Largest Number is : " + temp);
-        smallest();
+        smallest(dataArr);
     }
-    public static void smallest(){
+    public static void smallest(int[] dataArr){
         int temp = dataArr[1];
         for (int i = 2; i <= userInput; i++) {
             if (dataArr[i]<temp){
